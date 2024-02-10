e0ce6548ed832fc7d8fe39976f5c541e79c06e16
Bobwizardlad <bobwizardlad@gmail.com>
2024-02-10 13:56:12 -0600

    ✨Character Health Display
    Character's health is now displayed in a progress bar above the player sprite.

--- Dice-Temple/Player.gd
+++ Dice-Temple/Player.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@export var HEALTH_MAX: int
 @export var health: int
+@onready var health_display: Control = $ProgressBar
 
 func _ready():
-	pass
+	health_display.max_value = HEALTH_MAX
+
+func _process(delta):
+	health_display.value = health
 	
 func get_health() -> int:
 	return health
