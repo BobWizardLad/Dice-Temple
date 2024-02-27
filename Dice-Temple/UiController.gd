4e4063405ea138a9cea9c155c818a4a612e5cb1d
Bobwizardlad <bobwizardlad@gmail.com>
2024-02-27 11:40:32 -0600

    ✨💄🍱 Everything Else
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Dice-Temple/UiController.gd
+++ Dice-Temple/UiController.gd
@@ -4,12 +4,14 @@ func _on_unit_service_on_attack_roll(attack: Node2D):
 	$AttackName.text = attack.get_title()
 	$TurnHeader.visible = false # Hide turn menu
 	$AttackHeader.visible = true # Show reroll menu
+	$Rerolls.visible = true
 
 func _on_confirm():
 	$TurnHeader.visible = true # Show turn menu
 	$AttackHeader.visible = false # Hide reroll menu
 	$TurnHeader/Attack.modulate = Color(1, 1, 1, 0.33)
 	$AttackHeader/Reroll.modulate = Color(1, 1, 1, 1)
+	$Rerolls.visible = false
 
 func _on_game_service_on_reroll(attack: Node2D, rerolls: int):
 	$AttackName.text = attack.get_title()
@@ -23,7 +25,11 @@ func _on_turn_end_button_down():
 func _on_game_service_game_win():
 	$GameEnd.visible = true
 	$GameEnd/WinMessage.visible = true
+	$TurnHeader.visible = false # Hide turn menu
+	$AttackHeader.visible = false # Hide reroll menu
 
 func _on_game_service_game_over():
 	$GameEnd.visible = true
 	$GameEnd/LoseMessage.visible = true
+	$TurnHeader.visible = false # Hide turn menu
+	$AttackHeader.visible = false # Hide reroll menu
